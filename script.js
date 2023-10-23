7aa606d820584ab9aa0519307a8980e10e1e0eae
filippo <riccardo.gabba02@gmail.com>
2023-10-23 15:40:33 +0200

    printed in the console,log names, roles and pictures

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-const nomi = [
+const cards = [
 {
-    nome: "Wayne",
+    nome: "Wayne Barnett",
     role: "Founder & CEO",
     picture: "wayne-barnett-founder-ceo.jpg",
-}
+},
 {
     nome: "Angela Caroll",
     role: "Chief Editor",
     picture: "angela-caroll-chief-editor.jpg"
-}
+},
 {
     nome: "Walter Gordon",
     role: "Office Manager	",
     picture: "walter-gordon-office-manager.jpg"
-}
+},
 {
     nome: "Angela Lopez",
     role: "Social Media Manager",
     picture: "angela-lopez-social-media-manager.jpg"
-}
+},
 {
     nome: "Scott Estrada",
     role: "Developer",
     picture: "scott-estrada-developer.jpg"
-}
+},
 {
     nome: "Barbara Ramos",
     role: "Graphic Designer",
     picture: "barbara-ramos-graphic-designer.jpg"
 }
 ]
+
+for (let i = 0; i < cards.length; i++) {
+    console.log(cards[i]);
+   }
+   
