e2cf5d397460392db0d3b3dc0ad9a5b437f7705c
filippo <riccardo.gabba02@gmail.com>
2023-10-23 16:19:47 +0200

    completed i guess

--- script.js
+++ script.js
@@ -1,37 +1,58 @@
+"use strict"
+
 const cards = [
-{
-    nome: "Wayne Barnett",
-    role: "Founder & CEO",
-    picture: "wayne-barnett-founder-ceo.jpg",
-},
-{
-    nome: "Angela Caroll",
-    role: "Chief Editor",
-    picture: "angela-caroll-chief-editor.jpg"
-},
-{
-    nome: "Walter Gordon",
-    role: "Office Manager	",
-    picture: "walter-gordon-office-manager.jpg"
-},
-{
-    nome: "Angela Lopez",
-    role: "Social Media Manager",
-    picture: "angela-lopez-social-media-manager.jpg"
-},
-{
-    nome: "Scott Estrada",
-    role: "Developer",
-    picture: "scott-estrada-developer.jpg"
-},
-{
-    nome: "Barbara Ramos",
-    role: "Graphic Designer",
-    picture: "barbara-ramos-graphic-designer.jpg"
-}
+    {
+        nome: "Wayne Barnett",
+        role: "Founder & CEO",
+        picture: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        nome: "Angela Caroll",
+        role: "Chief Editor",
+        picture: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: "Walter Gordon",
+        role: "Office Manager	",
+        picture: "walter-gordon-office-manager.jpg"
+    },
+    {
+        nome: "Angela Lopez",
+        role: "Social Media Manager",
+        picture: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nome: "Scott Estrada",
+        role: "Developer",
+        picture: "scott-estrada-developer.jpg"
+    },
+    {
+        nome: "Barbara Ramos",
+        role: "Graphic Designer",
+        picture: "barbara-ramos-graphic-designer.jpg"
+    }
 ]
 
 for (let i = 0; i < cards.length; i++) {
-    console.log(cards[i]);
-   }
-   
+    printCol(cards[i]);
+}
+
+
+function printCol(cards) {
+    const col = document.createElement('div');
+    col.classList.add('col-4');
+    const template = `
+    <div class="card text-start">
+     <img class="card-img-top" src="img/${cards.picture}"
+     <div class="card-body">
+            <h4 class="card-title">${cards.name}</h4>
+            <p class="card-text">${cards.role}
+            </p>
+            </div>
+        </div>
+    `;
+    col.innerHTML = template 
+    const row = document.getElementById('row');
+
+    row.append('col')
+}
