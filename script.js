dfb0f8ee33e56bf8ab307118111c6f2eab178520
filippo <riccardo.gabba02@gmail.com>
2023-10-23 15:36:12 +0200

    finished MILESTONE 0

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 const nomi = [
-    
+{
+    nome: "Wayne",
+    role: "Founder & CEO",
+    picture: "wayne-barnett-founder-ceo.jpg",
+}
+{
+    nome: "Angela Caroll",
+    role: "Chief Editor",
+    picture: "angela-caroll-chief-editor.jpg"
+}
+{
+    nome: "Walter Gordon",
+    role: "Office Manager	",
+    picture: "walter-gordon-office-manager.jpg"
+}
+{
+    nome: "Angela Lopez",
+    role: "Social Media Manager",
+    picture: "angela-lopez-social-media-manager.jpg"
+}
+{
+    nome: "Scott Estrada",
+    role: "Developer",
+    picture: "scott-estrada-developer.jpg"
+}
+{
+    nome: "Barbara Ramos",
+    role: "Graphic Designer",
+    picture: "barbara-ramos-graphic-designer.jpg"
+}
 ]
